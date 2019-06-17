de43a1357bbd088f7a074b9c23e988ad7ea37984
akersof <akersof@gmail.com>
2019-06-17 03:31:13 +0200

    template of customised package.json and manifest.json

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
-console.log("Hello world");
+#!/usr/bin/env node
+const fs = require('fs-extra');
+
+//TODO: use commander:
+
+const templateDir = __dirname + '/templates/dapp';
+
+
+if(process.argv.length !== 3) {
+    //TODO: better error management
+    console.log("command line argument is wrong");
+    process.exit(1);
+}
+
+const appName = process.argv[2];
+
+const MANIFEST = {
+    "short_name": appName,
+    "name": appName,
+    "icons": [
+        {
+            "src": "favicon.ico",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
+        }
+    ],
+    "start_url": ".",
+    "display": "standalone",
+    "theme_color": "#000000",
+    "background_color": "#ffffff"
+};
+
+const PACKAGE_JSON = {
+    "name": appName,
+    "version": "0.1.0",
+    "private": true,
+    "dependencies": {
+        "ethers": "^4.0.29",
+        "react": "^16.8.6",
+        "react-dom": "^16.8.6",
+        "react-router-dom": "^5.0.1"
+    },
+    "scripts": {
+    },
+    "eslintConfig": {
+        "extends": "react-app"
+    },
+    "browserslist": {
+        "production": [
+            ">0.2%",
+            "not dead",
+            "not op_mini all"
+        ],
+        "development": [
+            "last 1 chrome version",
+            "last 1 firefox version",
+            "last 1 safari version"
+        ]
+    }
+};
+
+async function copyTemplate () {
+    try {
+        await fs.copy(templateDir, appName);
+        console.log('success!')
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+async function generateManifest() {
+    try {
+        await fs.outputFile(`${appName}/public/manifest.json`, JSON.stringify(MANIFEST));
+    } catch (err) {
+        console.error(error);
+    }
+}
+
+async function generatePackageJson() {
+    try {
+        await fs.outputFile(`${appName}/package.json`, JSON.stringify(PACKAGE_JSON));
+    } catch(err) {
+        console.error(error);
+    }
+}
+
+copyTemplate().then((_) => generateManifest()).then((_) => generatePackageJson());
---
+++ templates/dapp/src/Dapp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function Dapp() {
+    return (
+        <div className="App">
+            <p>Hello World</p>
+        </div>
+);
+}
+export default Dapp;
